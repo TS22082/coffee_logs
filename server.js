f80f4a6a1099ca9edac180e26fc4a8718d6a5602
TS22082 <ts22082@gmail.com>
2020-07-12 13:50:42 -0700

    finish server and model relationships

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const session = require("express-session");
+const db = require("./models");
+const PORT = process.env.PORT || 3000;
+const passport = require("./config/passport.js");
 const app = express();
 
 require("dotenv").config();
@@ -8,10 +11,20 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("./client"));
 
-app.secret(
+app.use(
   session({
     secret: process.env.SECRET,
     resave: true,
     saveUninitialized: true,
+    cookie: {
+      maxAge: 36 * 100000,
+    },
   })
 );
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+db.sequelize.sync().then(() => {
+  app.listen(PORT, () => console.log(`listening at: http://localhost:${PORT}`));
+});
