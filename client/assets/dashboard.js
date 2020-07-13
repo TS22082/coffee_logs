683a71a0284b9f9a17e94df49899484b51669a0f
TS22082 <ts22082@gmail.com>
2020-07-13 12:33:16 -0700

    set up layout log form and logs on dashboard

--- client/assets/dashboard.js
+++ client/assets/dashboard.js
@@ -1,6 +1,71 @@
 $(document).ready(function () {
-  $.ajax({
-    type: "GET",
-    url: "/auth/user",
-  }).then((res) => console.log(res));
+  const createLog = () => {
+    return new Promise((resolve, reject) => {
+      $.ajax({
+        type: "POST",
+        url: "/logs/new",
+        data: {
+          company: $("#companyInput").val().trim(),
+          roast: $("#roastInput").val().trim(),
+          name: $("#nameInput").val().trim(),
+          description: $("#descriptionInput").val().trim(),
+        },
+      }).then(() => {
+        $("#companyInput").val("");
+        $("#roastInput").val("");
+        $("#nameInput").val("");
+        $("#descriptionInput").val("");
+        logInstance.close();
+      });
+    });
+  };
+
+  const renderLogs = () => {
+    return new Promise((resolve, reject) => {
+      $.ajax({
+        type: "GET",
+        url: "/logs/user",
+      }).then((logs) => {
+        $("#logsContainer").empty();
+        logs.forEach((log) => {
+          let { name, company, roast, description } = log;
+          if (name) {
+            name = `<p>${name}</p>`;
+          } else {
+            name = "";
+          }
+          $("#logsContainer").append(`
+          <div class="row">
+            <div class="card brown darken-1">
+              <div class="card-content white-text">
+                ${name}
+                <span class="card-title">${company}</span>
+                <br>
+                <p>${roast}</p>
+                <p>${description}</p>
+              </div>
+            </div>
+          </div>
+          `);
+        });
+      });
+    });
+  };
+
+  $(".parallax").parallax();
+  $(".sidenav").sidenav();
+
+  const logModal = document.getElementById("newLogModal");
+  const logInstance = M.Modal.init(logModal, { dismissible: true });
+
+  renderLogs().then(() => console.log(success));
+
+  $("#newLogBtn").on("click", () => logInstance.open());
+  $("#logCancel").on("click", () => logInstance.close());
+
+  $("#logForm").on("submit", (e) => {
+    e.preventDefault();
+    createLog();
+    renderLogs();
+  });
 });
